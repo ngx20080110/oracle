0b7afe74fca825a3ac90b3ee86d6b7bf47fd69e2
ngx20080110 <ngx20080110@126.com>
2015-09-17 10:36:26 +0800

    Update ex_simple_case.sql

--- ex_simple_case.sql
+++ ex_simple_case.sql
@@ -31,5 +31,24 @@ BEGIN
     THEN
       DBMS_OUTPUT.PUT_LINE('Error [CASE_NOT_FOUND]');
   END;
+  
+  -- If the selector is NULL value, it cannot be matched with WHEN NULL.
+  grade := NULL;
+  BEGIN
+    CASE grade
+      WHEN 'A'
+      THEN
+        DBMS_OUTPUT.PUT_LINE('Excellent');
+      WHEN NULL
+      THEN
+        DBMS_OUTPUT.PUT_LINE('Null is match');
+      ELSE
+        DBMS_OUTPUT.PUT_LINE('Else case');
+    END CASE;
+  EXCEPTION
+    WHEN CASE_NOT_FOUND
+    THEN
+      DBMS_OUTPUT.PUT_LINE('Error [CASE_NOT_FOUND]');
+  END;
 END;
 /
